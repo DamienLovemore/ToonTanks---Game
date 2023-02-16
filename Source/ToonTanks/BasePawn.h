5c4781ec3e57af7bdb7e82fd2c13f504c87f4b23
DamienLovemore <matheusoaresmartins2020@gmail.com>
2023-02-16 08:05:46 -0300

    Forward Declaring

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -26,4 +26,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	//Forward declare a variable to make header file smaller
+	UPROPERTY()	
+	class UCapsuleComponent* CapsuleComp;
 };
